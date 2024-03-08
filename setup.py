ebab90d8c9b28a507891221a5c2034b929d07667
mahuan-git <mahuan15@mail.ustc.edu.cn>
2024-03-08 16:14:19 +0800

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    'pyscf',
+    'numpy',
+    'scipy',
+    'openfermion',
+]
+
+
+setup(name='ManyBodyExpansion',
+      version='0.1',
+      description='manybody expansion as a quantum chemistry fragmentation scheme',
+      author='Ma Huan',
+      author_email='mahuan15@mail.ustc.edu.cn',
+      packages=find_packages(),
+      install_requires=REQUIRED_PACKAGES,
+      platforms=['any'],
+     )
